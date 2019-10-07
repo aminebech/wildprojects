0c3d26845eb3a2cfa313cc9b4452c30be5431008
aminebech <amine.bechouaia@gmail.com>
2019-10-07 21:32:59 +0200

    maj quete react 2

--- quetes-react/simpsons-quotes-react/src/App.js
+++ quetes-react/simpsons-quotes-react/src/App.js
@@ -8,7 +8,15 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <QuoteCard />
+      <QuoteCard 
+      image='https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523' 
+      character='Ralph Wiggum'
+      quote="Me fail English? That's unpossible"
+     />
+      <QuoteCard
+      image='https://upload.wikimedia.org/wikipedia/en/c/c6/Nelson_Muntz.PNG' 
+      character='Nelson Muntz'
+      quote="Shoplifting is a victimless crime, like punching someone in the dark." />
     </div>
     
   );
