737c5c3eb99313adb1f494a95b31f2368cf8695e
aminebech <amine.bechouaia@gmail.com>
2019-10-07 19:56:36 +0200

    maj

--- quetes-react/simpsons-quotes-react/src/App.js
+++ quetes-react/simpsons-quotes-react/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
+    
   );
 }
+
 
 export default App;
---
+++ quetes-react/simpsons-quotes-react/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css';
+
+
+function Navbar() {
+    return (
+        <nav className="Navbar">
+            <a href="https://fr.wikipedia.org/wiki/The_Simpsons"> Simpons </a>
+
+        </nav>
+    )
+}
+
+export default Navbar;
